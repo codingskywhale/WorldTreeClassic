5328580706be9cb582522b5854e2bf520681d377
boguen <qhrms012@naver.com>
2025-04-18 20:01:20 +0900

    [add]빌드테스트 및 구글로그인을위한 앱번들 및 안드로이드 키 설정하기

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,7 +36,7 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "41547311661"; // Filled in automatically
+        public const string ApplicationId = "1094607343649"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
         public const string WebClientId = ""; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
